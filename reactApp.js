8295c0d5094f27f69ec56457913e127e871f3728
Manmohan7 <er.manmohansingh7@gmail.com>
2020-08-22 14:46:21 +0530

    feat: Saving data on database

--- reactApp.js
+++ reactApp.js
@@ -25,13 +25,12 @@ class Goals extends React.Component {
   addItem = (e) => {
     e.preventDefault()
 
-    const name = this.input.value
-    this.input.value = ''
-
-    this.props.store.dispatch(addGoalAction({
-      id: generateId(),
-      name
-    }))
+    API.saveGoal(this.input.value)
+      .then((goal) => {
+        this.props.store.dispatch(addGoalAction(goal))
+        this.input.value = ''
+      })
+      .catch(() => alert('Error occurred. Try again.'))
   }
 
   removeItem = (goal) => {
@@ -71,14 +70,12 @@ class Todos extends React.Component {
   addItem = (e) => {
     e.preventDefault()
 
-    const name = this.input.value
-    this.input.value = ''
-
-    this.props.store.dispatch(addTodoAction({
-      id: generateId(),
-      name,
-      complete: false
-    }))
+    API.saveTodo(this.input.value)
+      .then((todo) => {
+        this.props.store.dispatch(addTodoAction(todo))
+        this.input.value = ''
+      })
+      .catch(() => alert('Error occurred. Try again.'))
   }
 
   removeItem = (todo) => {
